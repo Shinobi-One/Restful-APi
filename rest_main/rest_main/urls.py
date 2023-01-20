701fa25c0ca4fbc857edf381a9ce433c047faa42
Hosein_ Vazife <116436157+Shinobi-One@users.noreply.github.com>
2023-01-20 15:21:49 +0330

    Update urls.py

--- rest_main/rest_main/urls.py
+++ rest_main/rest_main/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('rest_app1.urls')),
+    path('home',include('rest_app1.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
